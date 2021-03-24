babef5c7986f779b01f7efbcbf2a87281752ac76
Michael Maitland <mtm68@cornell.edu>
2021-03-23 22:19:40 -0400

    begin plumbing tests

--- ogit.ml
+++ ogit.ml
@@ -1,3 +1,3 @@
-let () = 
+(*let () = 
   let c = Command.parse Sys.argv in
-    Command.execute c
+    Command.execute c *)
--- test.ml
+++ test.ml
@@ -2,22 +2,27 @@ open OUnit2
 open Plumbing
 open Porcelain
 
-(** [init_test n a ch cl] constructs an OUnit test named [n] that 
+(** [plumbing_test n a ch cl] constructs an OUnit test named [n] that 
     asserts [ch] to check side effects and then calls [cl] which cleans
     up the side effects *)
-let init_test
+let plumbing_test
     (name : string)
+    (f : string list -> 'a)
     (args : string list)
-    (check_side_effect : fun unit -> bool)
-    (clean_side_effect : fun unit -> unit) : test =
+    (check_side_effect : unit -> bool)
+    (clean_side_effect : unit -> unit) : test =
   name >:: fun _ ->
-    Plumbing.init args;
-    assert_true check_side_effect;
-    clean_side_effect;
+    f args;
+    try
+      assert_bool "side effect did not occur" (check_side_effect ())
+    with Failure f -> 
+      clean_side_effect (); 
+      raise (Failure f)
 
 (** Tests for [Plumbing.init] *)
 let init_tests = [
-  init_test "init no args" []
+  plumbing_test "init no args" Plumbing.init [] (fun () -> Sys.file_exists ".git") (fun () -> Sys.remove ".git");
+  plumbing_test "init tmp" Plumbing.init ["tmp"] (fun () -> Sys.file_exists "tmp/.git") (fun () -> Sys.remove "tmp")
 ]
 
 (** Tests for [Plumbing.hash_object] *)
@@ -66,16 +71,15 @@ let diff_tests = [
 
 
 (** Tests for [Plumbing ] module *)
-let plumbing_tests = [
+let plumbing_tests =
   init_tests (*@ hash_object_tests @ cat_file_tests @ update_index_tests
   @ write_tree_tests @ read_tree_tests @ commit_tree_tests @ log_tests
   @ add_tests @ commit_tests @ show_tests @ diff_tests *)
-]
+
 
 (** Tests for [Porcelain] module *)
-let porcelain_tests = [
+let porcelain_tests = []
 
-]
 
 let suite =
   "test suite for ogit"
