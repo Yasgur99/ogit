4748d46593bd45d9281438f71ecbd622ec4b1fb1
Michael Maitland <mtm68@cornell.edu>
2021-03-23 20:19:56 -0400

    add unimplemented plumbing

---
+++ plumbing.ml
@@ -0,0 +1,36 @@
+let init (args : string list) =
+  failwith "unimplemented"
+
+let hash_object (args : string list) =
+  failwith "unimplemented"
+
+let cat_file (args : string list) =
+  failwith "unimplemented"
+
+let update_index (args : string list) =
+  failwith "unimplemented"
+
+let write_tree (args : string list) = 
+  failwith "unimplemented"
+
+let read_tree (args : string list) =
+  failwith "unimplemented"
+
+let commit_tree (args : string list) =
+  failwith "unimplemented"
+
+let log (args : string list) =
+  failwith "unimplemented"
+
+let add (args : string list) =
+  failwith "unimplemented"
+  
+let commit (args : string list) =
+  failwith "unimplemented"
+
+let show (args : string list) =
+  failwith "unimplemented"
+
+let diff (args : string list) =
+  failwith "unimplemented"
+ 
