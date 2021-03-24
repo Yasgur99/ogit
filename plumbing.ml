6764d5c825111e90628d364a047acd2b505ecdad
Michael Maitland <mtm68@cornell.edu>
2021-03-24 00:06:20 -0400

    some more plumbing

--- plumbing.ml
+++ plumbing.ml
@@ -1,36 +1,59 @@
-let init (args : string list) =
-  Sys.command "echo hello"
+let fork_and_execv (exe : string) (args : string array) : string list = 
+  let inp, out = Unix.pipe() in
+  let pid = Unix.fork () in
+  if pid = 0 
+  then
+    Unix.close inp;
+    Unix.dup2 out Unix.stdout;
+    Unix.dup2 out Unix.stderr;
+    Unix.close inp; 
+    Unix.execvp exe args
+  else 
+    let lines = ref [] in
+    Unix.close out;
+    try 
+      while true do
+        lines:= input_line inp :: !lines
+      done; !lines
+    with End_of_file ->
+      close_in
 
-let hash_object (args : string list) =
-  failwith "unimplemented"
+let init (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "init"|] args)
 
-let cat_file (args : string list) =
-  failwith "unimplemented"
+let hash_object (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "hash-object"|] args)
 
-let update_index (args : string list) =
-  failwith "unimplemented"
+let cat_file (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "cat-file"|] args)
 
-let write_tree (args : string list) = 
-  failwith "unimplemented"
+let update_index (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "update-index"|] args)
 
-let read_tree (args : string list) =
-  failwith "unimplemented"
+let write_tree (args : string array) = 
+  fork_and_execv "git" (Array.append [|"git"; "write-tree"|] args)
 
-let commit_tree (args : string list) =
-  failwith "unimplemented"
+let read_tree (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "read-tree"|] args)
 
-let log (args : string list) =
-  failwith "unimplemented"
+let commit_tree (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "commit-tree"|] args)
 
-let add (args : string list) =
-  failwith "unimplemented"
+let log (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "log"|] args)
+
+let add (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "add"|] args)
   
-let commit (args : string list) =
-  failwith "unimplemented"
+let commit (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "commit"|] args)
 
-let show (args : string list) =
-  failwith "unimplemented"
+let show (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "show"|] args)
 
-let diff (args : string list) =
-  failwith "unimplemented"
+let diff (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "diff"|] args)
+ 
+let status (args : string array) =
+  fork_and_execv "git" (Array.append [|"git"; "status"|] args)
  
--- plumbing.mli
+++ plumbing.mli
@@ -4,37 +4,40 @@
     any command line arguments *)
 
 (** [init args] calls git init with arguments [args] *)
-val init : string list -> unit
+val init : string array -> unit
 
 (** [hash_object args] calls git hash-object with arguments [args] and is the output to standard output *)
-val hash_object : string list -> string
+val hash_object : string array -> string
 
 (** [cat_file args] calls git cat-file with arguments [args] *)
-val cat_file : string list -> unit
+val cat_file : string array -> unit
 
 (** [update_index args] calls git update-index with arguments [args] *)
-val update_index  : string list ->  unit
+val update_index  : string array ->  unit
 
 (** [write_tree args] calls git write-tree with arguments [args] *)
-val write_tree : string list -> unit
+val write_tree : string array -> unit
 
 (** [read_tree args] calls git read-tree  with arguments [args] *)
-val read_tree : string list -> unit
+val read_tree : string array -> unit
 
 (** [commit_tree args] calls git commit-tree with arguments [args] *)
-val commit_tree : string list -> unit
+val commit_tree : string array -> unit
 
 (** [log args] calls git update-index with arguments [args] *)
-val log : string list -> unit 
+val log : string array -> unit 
 
 (** [add args] calls git add with arguments [args] *)
-val add : string list -> unit
+val add : string array -> unit
 
 (** [commit] calls git commit with arguments [args] *)
-val commit : string list -> unit
+val commit : string array -> unit
 
 (** [show args] calls git show with arguments [args] *)
-val show : string list  -> unit
+val show : string array  -> unit
 
 (** [diff args] calls git diff with arguments [args] *)
-val diff : string list -> unit
+val diff : string array -> unit
+
+(** [status args] calls git status with arguments [args] *)
+val status: string array -> unit
