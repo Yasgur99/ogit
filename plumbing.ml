cb5b6af24f0962bd521f14961dc3cd9fc87541cf
Michael Maitland <mtm68@cornell.edu>
2021-03-24 00:36:54 -0400

    make progres on fork and exec

--- plumbing.ml
+++ plumbing.ml
@@ -1,22 +1,24 @@
 let fork_and_execv (exe : string) (args : string array) : string list = 
   let inp, out = Unix.pipe() in
   let pid = Unix.fork () in
-  if pid = 0 
-  then
-    Unix.close inp;
-    Unix.dup2 out Unix.stdout;
-    Unix.dup2 out Unix.stderr;
-    Unix.close inp; 
+  if pid = 0 then
+    Unix.close inp; (* Not used by child *)
+    Unix.dup2 out Unix.stdout; (* Bind out pipe to stdout *)
+    Unix.dup2 out Unix.stderr; (* Bind out pipe to stderr *)
     Unix.execvp exe args
   else 
+    let inc = Unix.in_channel_of_descr inp in
     let lines = ref [] in
     Unix.close out;
     try 
       while true do
-        lines:= input_line inp :: !lines
-      done; !lines
+        lines:= input_line inc :: !lines
+      done;
+      !lines
     with End_of_file ->
-      close_in
+      close_in inc;
+      Unix.close inp;
+      !lines
 
 let init (args : string array) =
   fork_and_execv "git" (Array.append [|"git"; "init"|] args)
