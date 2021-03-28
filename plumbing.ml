6fa20bbc495773837d075d3dbabbd5cc1e4edbe2
Pat Lyons <pml84@cornell.edu>
2021-03-28 16:41:44 -0400

    merged porcelain files

--- plumbing.ml
+++ plumbing.ml
@@ -1,26 +1,18 @@
 (** Types and methods to access and construct the type *)
-type result = { 
+type result = {
   stdout : string list;
   stderr : string list;
-  out_and_err : string list;
-  (*exit_code : int;*)
+  out_and_err : string list; (*exit_code : int;*)
 }
 
 let make_result out err out_and_err =
-  {
-    stdout =  out;
-    stderr = err;
-    out_and_err = out_and_err;
-  }
+  { stdout = out; stderr = err; out_and_err }
 
-let get_stdout result = 
-  result.stdout
+let get_stdout result = result.stdout
 
-let get_stderr result =
-  result.stderr
+let get_stderr result = result.stderr
 
-let get_out result =
-  result.out_and_err
+let get_out result = result.out_and_err
 
 (** Helper Methods *)
 
@@ -28,85 +20,98 @@ let get_out result =
 let read (fd : Unix.file_descr) : string list =
   let in_ch = Unix.in_channel_of_descr fd in
   let lines = ref [] in
-  try 
-      while true do
-        lines:= input_line in_ch :: !lines
-      done;
-      !lines
-    with End_of_file ->
-      close_in in_ch; 
-      !lines
+  try
+    while true do
+      lines := input_line in_ch :: !lines
+    done;
+    !lines
+  with End_of_file ->
+    close_in in_ch;
+    !lines
 
-(** [fork_and_execvp e a] is the result of executing program [exe] 
-    with arguments [args]*)
-let fork_and_execv (exe : string) (args : string array) : result = 
-  let inp_stdout, out_stdout = Unix.pipe() in (* Pipe for stdout *)
-  let inp_stderr, out_stderr= Unix.pipe() in (* Pipe for stderr *)
-  let inp, out = Unix.pipe() in (* Pipe for both stdout and stderr *)
+(** [fork_and_execvp e a] is the result of executing program [exe] with
+    arguments [args]*)
+let fork_and_execv (exe : string) (args : string array) : result =
+  let inp_stdout, out_stdout = Unix.pipe () in
+  (* Pipe for stdout *)
+  let inp_stderr, out_stderr = Unix.pipe () in
+  (* Pipe for stderr *)
+  let inp, out = Unix.pipe () in
+  (* Pipe for both stdout and stderr *)
   let pid = Unix.fork () in
   if pid = 0 then (
-    Unix.close inp_stdout; (* Not used by child *)
-    Unix.close inp_stderr; (* Not used by child *)
-    Unix.close inp; (* Not used by child *)
-    Unix.dup2 out_stdout Unix.stdout; (* Bind stdout pipe to stdout *)
-    Unix.dup2 out_stderr Unix.stderr; (* Bind stderr pipe to stderr *)
-    Unix.dup2 out Unix.stdout; (* Bind out pipe to stdout *)
-    Unix.dup2 out Unix.stderr; (* Bind out pipe to stderr *)
-    Unix.execvp exe args 
-  ) else (
-    Unix.close out_stdout; (* Not used by parent*)
-    Unix.close out_stderr; (* Not used by parent*)
-    Unix.close out; (* Not used by parent*)
+    Unix.close inp_stdout;
+    (* Not used by child *)
+    Unix.close inp_stderr;
+    (* Not used by child *)
+    Unix.close inp;
+    (* Not used by child *)
+    Unix.dup2 out_stdout Unix.stdout;
+    (* Bind stdout pipe to stdout *)
+    Unix.dup2 out_stderr Unix.stderr;
+    (* Bind stderr pipe to stderr *)
+    Unix.dup2 out Unix.stdout;
+    (* Bind out pipe to stdout *)
+    Unix.dup2 out Unix.stderr;
+    (* Bind out pipe to stderr *)
+    Unix.execvp exe args)
+  else (
+    Unix.close out_stdout;
+    (* Not used by parent*)
+    Unix.close out_stderr;
+    (* Not used by parent*)
+    Unix.close out;
+    (* Not used by parent*)
     let stdout = read inp_stdout in
     let stdin = read inp_stderr in
     let out_and_err = read inp in
-      (* Does not close the pipes because [read fd] does that when
-         it closes the input channel it creates. 
+    (* Does not close the pipes because [read fd] does that when it
+       closes the input channel it creates.
 
-      Unix.close inp_stderr;
-      Unix.close inp_stdout;
-      Unix.close inp; *)
-      make_result stdout stdin out_and_err
-  )
+       Unix.close inp_stderr; Unix.close inp_stdout; Unix.close inp; *)
+    make_result stdout stdin out_and_err)
 
 let init (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "init"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "init" |] args)
 
 let hash_object (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "hash-object"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "hash-object" |] args)
 
 let cat_file (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "cat-file"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "cat-file" |] args)
 
 let update_index (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "update-index"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "update-index" |] args)
 
-let write_tree (args : string array) = 
-  fork_and_execv "git" (Array.append [|"git"; "write-tree"|] args)
+let write_tree (args : string array) =
+  fork_and_execv "git" (Array.append [| "git"; "write-tree" |] args)
 
 let read_tree (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "read-tree"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "read-tree" |] args)
 
 let commit_tree (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "commit-tree"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "commit-tree" |] args)
 
 let log (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "log"|] args)
+  fork_and_execv "git"
+    (Array.append [| "git"; "--no-pager"; "log" |] args)
 
 let add (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "add"|] args)
-  
+  fork_and_execv "git" (Array.append [| "git"; "add" |] args)
+
 let commit (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "commit"|] args)
+  fork_and_execv "git" (Array.append [| "git"; "commit" |] args)
 
 let show (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "show"|] args)
+  fork_and_execv "git"
+    (Array.append [| "git"; "--no-pager"; "show" |] args)
 
 let diff (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "--no-pager"; "diff"|] args)
- 
+  fork_and_execv "git"
+    (Array.append [| "git"; "--no-pager"; "diff" |] args)
+
 let status (args : string array) =
-  fork_and_execv "git" (Array.append [|"git"; "status"|] args)
- 
+  fork_and_execv "git" (Array.append [| "git"; "status" |] args)
+
 let git (args : string array) =
-  fork_and_execv "git" (Array.append [|"git";|] args)
+  fork_and_execv "git" (Array.append [| "git" |] args)
--- porcelain.ml
+++ porcelain.ml
@@ -1,45 +1,80 @@
 
-type commit_object = unit
+type commit_object = {
+  tree : string;
+  (* is this the SHA-1? *)
+  parents : string;
+  (* previous SHA-1 *)
+  author : string;
+  committer : string;
+  msg : string;
+}
 
-type object_id = string
+type object_id = { header_hash : string }
 
 type object_content = unit
 
-type object_type = unit
+type object_type =
+  | Blob of { contents : string }
+  | Tree of {
+      entry : int;
+      ob_type : object_type;
+      sha1 : string;
+      name : string;
+    }
+  | Commit of { com_ob : commit_object }
+  | Tag of {
+      obj_name : string;
+      (*?*)
+      ob_type : object_type;
+      tagger : string;
+      msg : string;
+    }
 
 type status_t = {
   untracked : string list;
   tracked : string list;
   staged : string list;
 }
+ 
 
-let init dir = failwith "unimplemented"
+let init dir : unit = failwith "unimplemented"
+  (* match dir with
+  | None -> Plumbing.init [||]
+  | Some _ -> Plumbing.init [| dir |] *)
 
-let hash_object file = failwith "unimplemented"
+  let hash_object file : object_id = failwith "unimplemented" (* Plumbing.hash_object [| "-w"; file |] *)
 
 let cat_file hash = failwith "unimplemented"
 
 let cat_file_type hash = failwith "unimplemented"
 
-let update_index hash file = failwith "unimplemented"
+let update_index hash file : unit = failwith "unimplemented"
+  (* Plumbing.update_index [| hash; file|] (* not sure of this one *) *)
 
-let write_tree () = failwith "unimplemented"
+let write_tree = failwith "Unimplemented"
 
-let read_tree hash = failwith "unimplemented"
+let read_tree hash = failwith "unimplemented" 
+(* Plumbing.read_tree [| hash |] *)
 
 let read_tree_prefix hash prefix = failwith "unimplemented"
+(* Plumbing.read_tree [| prefix; hash |] *)
 
 let commit_tree hash message = failwith "unimplemented"
-
+(* Plumbing.commit_tree [| hash; message |] *)
 let log hash = failwith "unimplemented"
+(* Plumbing.log [| hash |] *)
 
 let add files = failwith "unimplemented"
+(* Plumbing.add [| files |] *)
 
 let commit msg = failwith "unimplemented"
+(* Plumbing.commit [| "-m"; msg |] *)
 
 let show () = failwith "unimplemented"
+(* Plumbing.show [||] *)
 
 let diff () = failwith "unimplemented"
+(* Plumbing.diff [||] *)
 
 let status () = {
   (** TODO: This impl is just for mocking since it wasnt ready.
@@ -54,3 +89,5 @@ let get_untracked status = status.untracked
 let get_tracked status = status.tracked
 
 let get_staged status = status.staged 
+
+
