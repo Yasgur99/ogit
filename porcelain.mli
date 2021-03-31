4b09de38784b418ddd033e071328740861acb346
Pat Lyons <pml84@cornell.edu>
2021-03-31 17:04:42 -0400

    reformat porcelain.mli

--- porcelain.mli
+++ porcelain.mli
@@ -15,45 +15,56 @@ type object_type
 
 type status_t
 
-(** [init d] initializes a git repository in the current working directory if [n] is [None], otherwise initializes a git repository with name [n] as a subdirectory in the current working directory *)
+(** [init d] initializes a git repository in the current working
+    directory if [n] is [None], otherwise initializes a git repository
+    with name [n] as a subdirectory in the current working directory *)
 val init : string option -> unit
 
-(** [hash_object f] calls [Plumbing.hash_object] with command line argument -w,
-    which stores data in file with filename [f] in a repository's .git/objects
-    directory and is the object that refers to that data object *)
+(** [hash_object f] calls [Plumbing.hash_object] with command line
+    argument -w, which stores data in file with filename [f] in a
+    repository's .git/objects directory and is the object that refers to
+    that data object *)
 val hash_object : string -> object_id
 
-(** [cat_file h] calls git cat-file on object [h] with option -p, which displays the content of an object in the repository  *)
+(** [cat_file h] calls git cat-file on object [h] with option -p, which
+    displays the content of an object in the repository *)
 val cat_file : object_id -> object_content
 
-(** [cat_file_type h] calls git cat-file on object [h] with option -t, which displays the type of the object if [o] = -t *)
+(** [cat_file_type h] calls git cat-file on object [h] with option -t,
+    which displays the type of the object if [o] = -t *)
 val cat_file_type : object_id -> object_type
 
-(** [update_index h f] adds the object [h] with filename [f] to the staging area *)
+(** [update_index h f] adds the object [h] with filename [f] to the
+    staging area *)
 val update_index : object_id -> string -> unit
 
-(** [write_tree] writes the staging area out to a tree object and genereates a tree object from the state of the index if the tree does not yet exist *)
-val write_tree : unit -> object_id 
+(** [write_tree] writes the staging area out to a tree object and
+    genereates a tree object from the state of the index if the tree
+    does not yet exist *)
+val write_tree : unit -> object_id
 
 (** [read_tree h] reads tree object [h] into the staging area *)
 val read_tree : object_id -> unit
 
-(** [read_tree h p] reads tree object [h] into the staging area with prefix [p] *)
+(** [read_tree h p] reads tree object [h] into the staging area with
+    prefix [p] *)
 val read_tree_prefix : object_id -> string -> unit
 
-(** [commit_tree h m] creates and is a commit object from tree object [h] with commit message [m] *)
+(** [commit_tree h m] creates and is a commit object from tree object
+    [h] with commit message [m] *)
 val commit_tree : object_id -> string -> object_id
 
 (** [log h] is the list of commit objects that are reachable from HEAD
-    in reverse chronological order if [h] is [None], otherwise the commit
-    objects that are reachable by following parents of commit [h] in reverse
-    chronological order *)
+    in reverse chronological order if [h] is [None], otherwise the
+    commit objects that are reachable by following parents of commit [h]
+    in reverse chronological order *)
 val log : object_id option -> commit_object list
 
-(** [add fnames] adds the files with filenames [fnames] to the staging area *)
+(** [add fnames] adds the files with filenames [fnames] to the staging
+    area *)
 val add : string list -> unit
 
-(** [commit msg] commits the changes in the staging area with commit 
+(** [commit msg] commits the changes in the staging area with commit
     message [msg] *)
 val commit : string -> unit
 
@@ -67,10 +78,10 @@ val diff : unit -> unit
 val status : unit -> status_t
 
 (** [get_untracked s] is the untracked filenames in the status [s] *)
-val get_untracked: status_t -> string list
+val get_untracked : status_t -> string list
 
 (** [get_tracked s] is the untracked filenames in the status [s] *)
-val get_tracked : status_t -> string list 
+val get_tracked : status_t -> string list
 
 (** [get_staged s] is the staged filenames in the status [s] *)
 val get_staged : status_t -> string list
