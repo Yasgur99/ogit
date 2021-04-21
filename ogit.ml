d3a7bc3d2a6d183feca8e2e332c55952b6152679
db676 <db676@cornell.edu>
2021-04-21 16:02:39 -0400

    Test

--- ogit.ml
+++ ogit.ml
@@ -1,18 +1,26 @@
 let rec run win st =
   let msg =
     match State.get_mode st with
+    | State.CommitMode -> Renderer.render_commit_mode st win
+    | State.CommitDone ->
+        Renderer.render_commit_done st win;
+        ""
+    | State.CommitFailed ->
+        Renderer.render_commit_failed st win;
+        ""
     | State.Normal ->
         Renderer.render st win;
         ""
-    | State.CommitMode -> Renderer.render_commit_mode st win
   in
-  let key = Curses.wgetch win in
-  let cmd = Command.parse_key key in
-  let full_cmd =
-    match cmd with Command.Commit _ -> Command.Commit msg | _ -> cmd
+  let key =
+    if State.get_mode st <> CommitMode then Curses.wgetch win else 0
+  in
+  let cmd =
+    if State.get_mode st <> CommitMode then Command.parse_key key
+    else Command.Commit msg
   in
   let new_st = State.update_mode st cmd in
-  run win (State.exec new_st full_cmd)
+  run win (State.exec new_st cmd)
 
 let run_git args =
   List.iter print_endline (Plumbing.get_out (Plumbing.git args))
